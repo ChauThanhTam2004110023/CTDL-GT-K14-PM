98be6e07867ed8d25ec4ebf22ffa8bdc26f7459a
ChauThanhTam2004110023 <tamc5985@gmail.com>
2021-10-31 21:15:22 +0700

    Them class tuan 08

--- src/com/tamct200411023/tuan08/ProgrammingDeque.java
+++ src/com/tamct200411023/tuan08/ProgrammingDeque.java
@@ -1,28 +1,22 @@
 package com.tamct200411023.tuan08;
 
-import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 
 public class ProgrammingDeque {
     public static void main(String[] args) {
-        Deque<String> D = new ArrayDeque<String>();
+        Deque<String> D = new LinkedList<String>();
         D.offer("TITV 1");
         D.offer("Nguyen Van A");
         D.offer("Nguyen Van B");
         D.offer("TITV 2");
         D.offer("TITV 4");
-        D.offer("TITV 0");
+        //D.offer("TITV 0");
 
-        while(true)
-        {
-            String ten = D.poll();
-            {
-                if(ten == null)
-                {
-                    break;
-                } 
-            }
-            System.out.println(ten);
-        }
+        System.out.println(D);
+        System.out.println(D.pop());
+        System.out.println(D.poll());
+        System.out.println(D.pollFirst());
+        System.out.println(D.pollLast());
     }
 }
