23c7a688fb758030fd047ec4dddec82701927e79
ChauThanhTam2004110023 <tamc5985@gmail.com>
2021-11-06 17:50:46 +0700

    thêm class tuan 07

--- src/vn/edu/giadinh/tuan07/TestDrive.java
+++ src/vn/edu/giadinh/tuan07/TestDrive.java
@@ -3,9 +3,18 @@ package vn.edu.giadinh.tuan07;
 public class TestDrive {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
+        System.out.println("-----MENU-----");
+        System.out.println("1.NHAP THONG TIN");
+        System.out.println("2.IN DANH SACH");
+        System.out.println("3.TIM KIEM DAU DANH SACH");
+        //System.out.println("4.TIM KIEM CUOI DANH SACH");
+        System.out.println("4.XÓA ĐẦU DANH SÁCH");
+
+
         linkedList.nhapThongTin();
         linkedList.inDanhSach();
         linkedList.timKiemDauDS();
-        linkedList.timKiemCuoiDS();
+        //linkedList.timKiemCuoiDS();
+        linkedList.xoaDauDS();
     }
 }
